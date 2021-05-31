eb28e48a39eba2785adc95b52272298dac13ad3d
kn436428 <kn436428@dal.ca>
2021-05-31 13:15:26 -0300

    Deleted Redundant main.dart file

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy 2.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/kanakprajapati/Desktop/X691/Navu/lobster-detection/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/kanakprajapati/Desktop/X691/Navu/lobster-detection/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.3.sdk/usr/include/mach-o/dyld.modulemap
