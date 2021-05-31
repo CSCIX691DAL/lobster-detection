edeee9aac08891ea5c3e07b388c5b6a5e816f94b
kn436428 <kn436428@dal.ca>
2021-05-31 12:50:44 -0300

    Added Flutter Main page

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/kanakprajapati/Desktop/X691/Navu/lobster-detection/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
