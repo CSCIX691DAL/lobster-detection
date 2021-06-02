f5c44db6d7ea2535424c80e6e45bb451f1c9c0f8
osler <osler@cs.dal.ca>
2021-06-02 11:55:59 -0300

    Uploading camera flutter app

---
+++ flutter-app/flutter_camera_app-master/lib/screens/stack.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
