a415c8986ae68cf32c26975d5e18d98e2e0a3c45
Kanak Prajapati <33734672+kanak3699@users.noreply.github.com>
2021-06-14 13:36:28 -0300

    Update main.dart
    added import 'package:lobster_detection/firstScreen.dart';

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:lobster_detection/HomePage/splashPage.dart';
 import 'dart:async';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
+import 'package:lobster_detection/firstScreen.dart';
 import 'HomePage/MyHomePage.dart';
 
 void main() {
